1eac87c761abdad99385fc6dd28236dc18ddbf32
isabela-j <jugariu.isabela812@gmail.com>
2021-11-13 21:47:20 +0200

    login modified

--- events_app/frontend/src/Components/Login/login.js
+++ events_app/frontend/src/Components/Login/login.js
@@ -1,37 +1,58 @@
-import React, { Fragment } from 'react';
-import styles from './login.css';
+import React, { Fragment } from "react";
+import styles from "./login.css";
 
-const Login = () => 
-{
-    return (
-        <main class="pa4 black-80">
-            <form class="measure center">
-                <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-                    <legend class="f4 fw6 ph0 mh0 center">Log In</legend>
-                    <div class="mt3">
-                        <label class="db fw6 lh-copy f6" for="email-address">Email</label>
-                        <input class="pa2 input-reset ba bg-transparent hover-purple hover-white w-100" type="email" name="email-address" id="email-address"/>
-                    </div>
-                    <div class="mv3">
-                        <label class="db fw6 lh-copy f6" for="password">Password</label>
-                        <input class="b pa2 input-reset ba bg-transparent hover-purple hover-white w-100" type="password" name="password" id="password"/>
-                    </div>
-                    <div class="next">
-                        <label class="pa0 ma0 lh-copy white f6 pointer"><input type="checkbox"/> Remember me</label>
-                        <a href="#0" class="f6 link dim white underline db">Forgot your password?</a>
-                    </div>
-                </fieldset>
-                <div class="center">
-                    <input class="b ph3 pv2 input-reset ba b--purple bg-purple grow pointer f6 dib" type="submit" value="Log in"/>
-                </div>
-                <div class="lh-copy mt3 next center">
-                    <label class="db fw6 lh-copy f6 ">Don't have an account? </label>
-                    <a href="#0" class="f6 link dim white underline db"> Sign in.</a>
-                </div>
-            </form>
-        </main>
-
-    );
-}
+const Login = () => {
+  return (
+    <div class="pa4 black-80 content">
+      <form class="measure center">
+        <fieldset id="log_in" class="ba b--transparent center ph0 mh0">
+          <legend class="f4 fw6 ph0 white center2">Log in</legend>
+          <div class="mt3 center">
+            <input
+              class="pa2 fw4 f5 input-reset ba bg-transparent b--purple hover-white white-input w-100"
+              type="email"
+              name="email-address"
+              id="email-address"
+              placeholder="Email"
+            />
+          </div>
+          <div class="mv3 center">
+            <input
+              class="pa2 fw4 f5 input-reset ba bg-transparent b--purple hover-white white-input w-100"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+          <div class="lh-copy mt3 next center">
+            <label class="fw4 lh-copy white f5 pointer">
+              <input type="checkbox" /> Remember me
+            </label>
+            <a href="#0" class="fw4 link dim white underline f5 db">
+              Forgot your password?
+            </a>
+          </div>
+        </fieldset>
+        <div class="lh-copy mt3 center">
+          <input
+            class="ph3 pv2 fw4 ba b--purple bg-purple white grow pointer f5 dib mrg-left-8"
+            type="submit"
+            value="Log in"
+            
+          />
+        </div>
+        <div class="lh-copy mt3 next center">
+          <label class=" fw4 center lh-copy white f5 ">
+            Don't have an account?
+          </label>
+          <a href="#0" class="fw4 link dim white underline f5 db">
+            Sign in.
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
