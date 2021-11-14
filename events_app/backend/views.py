cd286333d7869bc1826ffd32059a7a117a879df4
Alex Dzen <dzenalex9@gmail.com>
2021-11-14 22:30:12 +0200

    progrese backend

--- events_app/backend/views.py
+++ events_app/backend/views.py
@@ -12,8 +12,21 @@ from .serializers import *
 
 from .models import *
 from rest_framework import status
+import json
 
 # Create your views here.
+
+@api_view(['POST'])
+def login(request):
+    
+    data = json.loads(request.body.decode('utf-8'))
+    if request.method == "POST":
+        try:
+            user = User.objects.get(email = data["email"],password = data["password"])
+            return Response({'exists':True})
+        except User.DoesNotExist as er:
+            print("Error: ",er)
+            return Response({'error':str(er)},status=status.HTTP_400_BAD_REQUEST)
 
 class UserView(viewsets.ModelViewSet):
     # create a sereializer class and
--- events_app/frontend/src/Components/Login/login.js
+++ events_app/frontend/src/Components/Login/login.js
@@ -49,13 +49,13 @@ class Login extends React.Component {
 
   render(){
     return (
-      <div class="pa4 black-80 content">
-        <form class="measure center" method="POST">
-          <fieldset id="log_in" class="ba b--transparent center ph0 mh0">
-            <legend class="f4 fw6 ph0 white center2">Log in</legend>
-            <div class="mt3 center">
+      <div className="pa4 black-80 content">
+        <form action="http://localhost:8000/api/tasks/" className="measure center" method="post">
+          <fieldset id="log_in" className="ba b--transparent center ph0 mh0">
+            <legend className="f4 fw6 ph0 white center2">Log in</legend>
+            <div className="mt3 center">
               <input
-                class="pa2 fw4 f5 input-reset ba bg-transparent b--purple hover-white white-input w-100"
+                className="pa2 fw4 f5 input-reset ba bg-transparent b--purple hover-white white-input w-100"
                 type="email"
                 name="email-address"
                 id="email-address"
@@ -63,9 +63,9 @@ class Login extends React.Component {
                 onChange = {this.inputChangeEmail}
               />
             </div>
-            <div class="mv3 center">
+            <div className="mv3 center">
               <input
-                class="pa2 fw4 f5 input-reset ba bg-transparent b--purple hover-white white-input w-100"
+                className="pa2 fw4 f5 input-reset ba bg-transparent b--purple hover-white white-input w-100"
                 type="password"
                 name="password"
                 id="password"
@@ -73,26 +73,28 @@ class Login extends React.Component {
                 onChange = {this.inputChangePassword}
               />
             </div>
-            <div class="lh-copy mt3 next center">
-              <label class="fw4 lh-copy white f5 pointer">
+            <div className="lh-copy mt3 next center">
+              <label className="fw4 lh-copy white f5 pointer">
                 <input type="checkbox" /> Remember me
               </label>
-              <a href="#0" class="fw4 link dim white underline f5 db">
+              <a href="#0" className="fw4 link dim white underline f5 db">
                 Forgot your password?
               </a>
             </div>
           </fieldset>
-          <div class="lh-copy mt3 center">
-            <button
-              class="ph3 pv2 fw4 ba b--purple bg-purple white grow pointer f5 dib mrg-left-8"
+          <div className="lh-copy mt3 center">
+            <input
+              className="ph3 pv2 fw4 ba b--purple bg-purple white grow pointer f5 dib mrg-left-8"
               onClick={this.weDoLoginFancy}
-            />Log in
+              type="submit"
+              value="Log In"
+            />
           </div>
-          <div class="lh-copy mt3 next center pad-left">
-            <label class=" fw4 center lh-copy white f5 ">
+          <div className="lh-copy mt3 next center pad-left">
+            <label className=" fw4 center lh-copy white f5 ">
               Don't have an account?
             </label>
-            <a href="#0" class="fw4 link dim white underline f5 db">
+            <a href="#0" className="fw4 link dim white underline f5 db">
               Sign in.
             </a>
           </div>
