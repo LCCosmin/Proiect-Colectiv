33a0ea2b3d9daf3b034562e8f3d0490eebc3842d
isabela-j <jugariu.isabela812@gmail.com>
2021-11-15 18:32:15 +0200

    login-signin

--- events_app/frontend/src/Components/Login/login.js
+++ events_app/frontend/src/Components/Login/login.js
@@ -1,52 +1,55 @@
-import React, { Fragment } from "react";
+import React from "react";
 import styles from "./login.css";
 
-const Login = () => {
+const Login = (props) => {
   return (
-    <div class="pa4 black-80 content">
-      <form class="measure center">
-        <fieldset id="log_in" class="ba b--transparent center ph0 mh0">
-          <legend class="f4 fw6 ph0 white center2">Log in</legend>
-          <div class="mt3 center">
+    <div className="pa4 black-80 content asd">
+      <form className="measure centram">
+        <fieldset id="log_in" className="ba b--transparent centram ph0 mh0">
+          <legend className="f4 fw6 ph0 white mrg-left-30">Log in</legend>
+          <div className="mt3 centram">
             <input
-              class="pa2 fw4 f5 input-reset ba bg-transparent b--purple hover-white white-input w-100"
+              className="pa2 fw4 f5 input-reset ba bg-transparent b--purple hover-white gray-input white-input w-100"
               type="email"
               name="email-address"
               id="email-address"
               placeholder="Email"
             />
           </div>
-          <div class="mv3 center">
+          <div className="mv3 centram">
             <input
-              class="pa2 fw4 f5 input-reset ba bg-transparent b--purple hover-white white-input w-100"
+              className="pa2 fw4 f5 input-reset ba bg-transparent b--purple hover-white white-input w-100"
               type="password"
               name="password"
               id="password"
               placeholder="Password"
             />
           </div>
-          <div class="lh-copy mt3 next center">
-            <label class="fw4 lh-copy white f5 pointer">
+          <div className="lh-copy mt3 centram">
+            <label className="fw4 lh-copy white f5 pointer">
               <input type="checkbox" /> Remember me
             </label>
-            <a href="#0" class="fw4 link dim white underline f5 db">
+            <a href="#0" className="fw4 link dim white underline f5 db">
               Forgot your password?
             </a>
           </div>
         </fieldset>
-        <div class="lh-copy mt3 center">
+        <div className="lh-copy mt3">
           <input
-            class="ph3 pv2 fw4 ba b--purple bg-purple white grow pointer f5 dib mrg-left-8"
+            className="ph3 pv2 fw4 ba b--purple bg-purple white grow pointer f5 dib mrg-left-16"
             type="submit"
             value="Log in"
-            
           />
         </div>
-        <div class="lh-copy mt3 next center">
-          <label class=" fw4 center lh-copy white f5 ">
+        <div className="lh-copy mt3 centram">
+          <label className=" fw4 centram lh-copy white f5 mrg-left-24">
             Don't have an account?
           </label>
-          <a href="#0" class="fw4 link dim white underline f5 db">
+          <a
+            href="#0"
+            className="fw4 link dim white underline f5 db mrg-left-32"
+            onClick={props.onDontHasUser}
+          >
             Sign in.
           </a>
         </div>
