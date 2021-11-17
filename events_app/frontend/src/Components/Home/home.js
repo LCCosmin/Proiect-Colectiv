2793778813b03f14126fdcc172ecc7c35971f310
cotoiamalia <81448356+cotoiamalia@users.noreply.github.com>
2021-11-17 22:33:11 +0200

    AmaliaC Update

--- events_app/frontend/src/Components/Home/home.js
+++ events_app/frontend/src/Components/Home/home.js
@@ -1,20 +1,19 @@
-import React, { Fragment } from 'react';
-import styles from './home.module.css';
-const Home = () => 
-{
-    return (
-        <Fragment className={styles.p}>
-            <p>
-                Hai ca merge
-            </p>
-            <p className={styles.merge}>
-                merge bine rau.
-            </p>
-            <p >
-                iurhgiruhg
-            </p>
-        </Fragment>  
-    );
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+  return ( 
+      <div className="home-container">
+          <div className="descriere">
+                <h1>
+                    ALL <span className="color1">EVENTS</span><br /> IN THE CITY <br />IN <span className="color2">ONE</span> PLACE
+                </h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt volutpat mollis. Mauris vitae mauris quis velit scelerisque mattis. Nam aliquam nisl ut leo elementum, ac pretium nibh consequat. Duis a dapibus nibh. Proin in sollicitudin quam, at cursus nisi. Nulla sollicitudin metus dolor, bibendum eleifend ante facilisis vitae.</p>
+                <button className="new-account">Make an account now !</button>
+            </div>
+      </div>
+  );
 }
 
 export default Home;
