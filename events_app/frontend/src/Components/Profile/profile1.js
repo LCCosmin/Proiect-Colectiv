da7d676e557b4a4656befc8c2573ded48483d4a0
Razvan Bartes <razvan.bartes2000@yahoo.com>
2021-12-04 13:00:45 +0200

    profile

--- events_app/frontend/src/Components/Profile/profile1.js
+++ events_app/frontend/src/Components/Profile/profile1.js
@@ -11,11 +11,11 @@ export default class Profile extends Component {
                 <article class="dark-gray mv4 w-50-m w-25-l">
                 <img src="http://placekitten.com/g/600/300" id="w-100" alt="Photo of a kitten looking menacing."/>
                 <div id="circle"></div>
-                    <div class='details'>
-                      <h1>Nume Prenume</h1>
-                      <h3>User/Organizator</h3>
-                      <h1>Varsta</h1>
-                      <h1>BIO</h1>
+                    <div id="details">
+                      <h1 id = "details">Nume Prenume</h1>
+                      <h3 id = "details">User/Organizator</h3>
+                      <h1 id = "details">Varsta</h1>
+                      <h1 id = "details">BIO</h1>
                       <p className ='description'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
                           Aenean commodo ligula eget dolor. Aenean massa. 
                           Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
@@ -23,7 +23,7 @@ export default class Profile extends Component {
                           <div className = "icons">
                           <h3 className="fandi"> <FaFacebookSquare/> </h3>
                           <h3 className="fandi"> <BsInstagram/> </h3>
-                          <button className="button">Connect</button>
+                          <button className="buttonConnect">Connect</button>
                           </div>
                     </div>
               </article>
