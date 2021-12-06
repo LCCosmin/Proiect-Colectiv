f67efb7dfaaf8e01ce4f8ed10c8efa01713dfd07
TeodoraMM <teodora_mezei12@yahoo.com>
2021-12-06 12:34:30 +0200

    updated home

--- events_app/frontend/src/Components/Home/Home.js
+++ events_app/frontend/src/Components/Home/Home.js
@@ -6,7 +6,7 @@ const Home = ({onRouteChange}) => {
   return ( 
       <div className="home-container">
           <div className="descriere">
-                <h1>
+                <h1 className="h1-Home">
                     ALL <span className="colorHome1">EVENTS</span><br /> IN THE CITY <br />IN <span className="colorHome2">ONE</span> PLACE
                 </h1>
                 <p className="font-home-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt volutpat mollis. Mauris vitae mauris quis velit scelerisque mattis. Nam aliquam nisl ut leo elementum, ac pretium nibh consequat. Duis a dapibus nibh. Proin in sollicitudin quam, at cursus nisi. Nulla sollicitudin metus dolor, bibendum eleifend ante facilisis vitae.</p>
